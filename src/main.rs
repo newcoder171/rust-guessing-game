a8950f1e0394b37b251407fc43a6c6b80a2bdc30
newcoder <newcoder17@gmail.com>
2021-11-03 15:40:53 +0100

    refactor to use enum

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use rand::prelude::*;
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    let max_tries = 5;
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     println!("I'm thinking of a number between 1 and 100...");
@@ -15,15 +17,18 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Failed to parse your guess.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-        if guess > secret_number {
-            println!("\n{} is too high! Go lower:", guess)
-        } else if guess < secret_number {
-            println!("\n{} is too low! Go higher:", guess)
-        } else {
-            println!("\nYou got it! The secret number was {}.", secret_number);
-            break;
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("\n{} is too low! Go higher:", guess),
+            Ordering::Greater => println!("\n{} is too high! Go lower:", guess),
+            Ordering::Equal => {
+                println!("\nYou got it! The secret number was {}.", secret_number);
+                break;
+            }
         }
     }
 }
